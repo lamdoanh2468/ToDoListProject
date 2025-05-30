bf7c8307f1c986762ad4a1966c921271b38f9753
lamdoanh2468 <lamdoanh2468@gmail.com>
2025-05-30 13:19:22 +0700

    30% done

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const toDoList = document.getElementById("todoList");
 var countTask = 0;
 const h2Task = document.getElementById("total-task");
 const totalText = h2Task.innerText;
-let tasks =[];
+let tasks = [];
 //Load savedTasks from local storage -> Convert string to JSON
 function loadStorage() {
     window.onload = () => {
         const savedTasks = localStorage.getItem("tasks");
-        if(savedTasks){
+        if (savedTasks) {
             tasks = JSON.parse(savedTasks);
             renderTask();
-        }     
+        }
     }
 }
 loadStorage();
@@ -32,7 +32,7 @@ function addTask() {
 }
 //Save task into LOCAL STORAGE
 function saveTask() {
-    localStorage.setItem("task  s", JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 //Use LocalStorage
 function renderTask() {
@@ -60,7 +60,7 @@ function renderTask() {
         //Delete Event
         delBtn.addEventListener("click", (e) => {
             e.stopPropagation();
-            const wannaDelete = window.confirm("Do you want to delete this task");
+            const wannaDelete = window.confirm("Do you want to remove this task");
             if (wannaDelete) {
                 tasks.splice(index, 1);
                 saveTask();
@@ -113,4 +113,46 @@ toDoInput.addEventListener("mouseenter", () => {
 const turnOffInput = toDoInput.addEventListener("mouseout", () => {
     toDoInput.placeholder = "";
 });
+//Change the title of to-do list
+const mainTitle = document.querySelector(".main-title");
+if (mainTitle != null) {
+    mainTitle.addEventListener("click", () => {
+        const editTitle = prompt("What title do you want to ?")
+        mainTitle.innerText = editTitle;
+    });
+} else {
+    console.warn("System Error:(");
+}
+//Handling Selecting Background image
+const openModalBtn = document.getElementById("openModal");
+const modal = document.getElementById("modal");
+const closeModalBtn = document.getElementById("closeModal");
+const images = document.querySelectorAll(".image-options img");
+//
+openModalBtn.addEventListener("click", () => {
+    modal.classList.remove("hidden","hide");
+    modal.classList.add("show");
+})
+closeModalBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
+    modal.classList.add("hide");
+})
+modal.addEventListener("animationend", function handleHide(e) {
+    if (e.animationName === "fadeOutScale") {
+        modal.classList.add("hidden");
+        // modal.removeEventListener("animationend", handleHide);
+    }
+});
+images.forEach((img) => {
+    img.addEventListener("click", () => {
+        const url = img.getAttribute("data-url");
+        document.body.style.backgroundImage = `url(${url})`;
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundPosition = "center";
+    })
+}
+);
+
+
+
 
